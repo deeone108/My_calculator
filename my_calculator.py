cc599ce887736e208cfe7aab826f9978d8d0427a
OJO DANIEL <79810426+deeone108@users.noreply.github.com>
2024-11-03 13:51:42 +0000

    5

--- my_calculator.py
+++ my_calculator.py
@@ -1,4 +1,4 @@
-# my_calculator.py by OJO DANIEL.
+# my_calculator.py by OJO DANIEL..
 # TODO: Make it work for all floating point numbers too
 
 if 3/2 == 1:  # Because Python 2 does not know maths
